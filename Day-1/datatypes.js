17d9d71292acd61d9fa82dba4a786c8ee59a06e1
Burak Şenocak <snckbrk@gmail.com>
2022-11-14 18:30:06 +0300

    Day-1-Exercises

---
+++ Day-1/datatypes.js
@@ -0,0 +1,61 @@
+// #STEP-5
+// Created this JavaScript file which is .datatypes.js
+// We created this variable.js in #STEP-4
+let carName = "Taycan"; // declared string variable
+let isCarCheap = false; //  declared boolean variable
+let priceOfCar = undefined; // undefined
+let brandOfCar = null; // null
+
+console.log(typeof carName); // checking data type
+console.log(typeof isCarCheap); // checking data type
+console.log(typeof priceOfCar); // checking data type
+console.log(typeof brandOfCar); // checking data type
+
+// #STEP-6
+//Declaring four variables without assigning values
+
+let laptopName;
+let laptopModel;
+let laptopPrice;
+let laptopBrand;
+
+// #STEP-7
+//Declaring four variables with assigned values
+
+laptopName = "MacBook";
+laptopModel = "Pro";
+laptopPrice = undefined;
+laptopBrand = "Apple";
+
+console.log(laptopName, laptopModel, laptopPrice, laptopBrand); // these codes for see the results at console
+
+// #STEP-8
+// Declaring variables to store your first name, last name, marital status, country and age in multiple lines
+
+// let firstName = "Burak";
+// let lastName = "Şenocak";
+// let maritalStatus = false;
+// let country = "Turkey";
+// let age = 25;
+
+// these codes for see the results at console
+// console.log(firstName, lastName, maritalStatus, country, age);
+
+// #STEP-9
+//Declaring variables to store your first name, last name, marital status, country and age in a single line
+
+let firstName = "Burak",
+  lastName = "Şenocak",
+  maritalStatus = false,
+  country = "Turkey",
+  age = 25;
+
+// these codes for see the results at console
+console.log(firstName, lastName, maritalStatus, country, age);
+
+// #STEP-10
+let myAge = "I am 25 years old.";
+let yourAge = "You are 30 years old.";
+
+console.log(myAge);
+console.log(yourAge);
---
+++ Day-1/variable.js
@@ -0,0 +1,18 @@
+// #STEP-1
+// Comments can make code readable
+
+// #STEP-2
+// Welcome to 30DaysOfJavaScript
+
+// #STEP-3
+/* 
+  Comments can make code readable,
+  Easy to reuse and informative
+*/
+
+// #STEP-4
+// We created this variable.js file
+let coffeeName = "Latte"; // declared string variable
+let isCoffeeHot = true; //  declared boolean variable
+let priceOfCoffee = undefined; // undefined
+let brandOfCoffee = null; // null
